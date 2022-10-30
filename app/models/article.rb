f956dcf94c939cc19f62119ffa4ce0fa1190c395
MercyChebet <chebetm96@gmail.com>
2022-10-30 23:29:48 +0300

    Completed challenge

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,26 @@
 class Article
 
-
-end
+    attr_accessor :magazine, :title#print all magazines
+  
+    @@all = []
+  
+    def initialize(author,magazine,title)
+      @author = author
+      @magazine = magazine
+      @title = title
+      @@all << self
+    end
+  
+    def title
+      @title
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    #print all authors
+    def author
+      @author
+    end
+  end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,56 @@
 class Magazine
   attr_accessor :name, :category
 
+  @@all = []
+
 
   def initialize(name, category)
     @name = name
     @category = category
+    @@all << self
 
   end
 
+  def name
+    @name
+  end
 
+  def category
+    @category
+  end
+
+  def self.all
+    @@all
+  end
+
+  def contributors
+    magazine_article = Article.all.select do |article|
+      article.magazine == self
+    end
+    magazine_article.map do |article|
+      article.author
+    end
+  end
+
+  def find_by_name(name)
+    Magazine.all.find do |magazine|
+      magazine.name == name
+    end
+  end
+
+  def artile_titles
+    arr = Article.all.select do |article|
+      article.magazine == self
+    end
+    arr.map do |article|
+      article.title
+    end
+  end
+
+  def contributing_authors
+    arr = contributors.tally
+    arr.select do |author, count|
+      count > 2
+    end
+  end
 end
